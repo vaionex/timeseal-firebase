b9e1b79152df21656862fe136d102d57d7ee7e1c
Saad <saad@vaionex.com>
2023-07-17 22:41:01 +0500

    fix: use axios instead of fetch

--- functions/timeseal.js
+++ functions/timeseal.js
@@ -1,16 +1,15 @@
 import config from "./config.js";
-import fetch from "node-fetch"
+import axios from "axios"
 
 export async function timestamp({ data, type }) {
-  const response = await fetch("https://api.timeseal.com/api/v1/timeseal", {
-    method: "POST",
-    body: JSON.stringify({ data, type }),
-    headers: new Headers({
-      "Content-Type": "application/json",
-      apiKey: config.API_KEY,
-    }),
-  });
-  const { txIds = [], status, msg } = await response.json();
+  const response = await axios.post("https://api.timeseal.com/api/v1/timeseal", { data, type },
+    {
+      headers: {
+        apiKey: config.API_KEY,
+      }
+    }
+  );
+  const { txIds = [], status, msg } = response.data
 
   if (status === "error") throw new Error(msg);
 
